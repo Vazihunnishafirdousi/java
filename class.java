1ca5dcdd7082cd023a0c40aaae838f15aa15c721
Vazihunnisha <artwithkhushi@213gmail.com>
2025-04-08 14:08:09 +0530

    msg

---
+++ class.java
@@ -0,0 +1,11 @@
+public class calcuation{
+    public static void main(String[] args){
+        System.out.println(x:"Calculation");
+
+        Scanner sc= new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum= a+b;
+        System.out.println("sum is"+sum);
+    }
+}
--- index.java
+++ index.java
@@ -2,6 +2,7 @@
  public class index{
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
     }
 
 }
